138b0c61875bd93bdbff060de0e5aabc88c40c4d
Varga Denis <vargadenisz@gmail.com>
2023-08-28 12:47:52 +0300

    Initial Commit

---
+++ gagstoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gagstoryboard
+//
+//  Created by Denis Istvan Varga on 2023. 08. 28..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
